75b2e7fd79fd67add148d181298b7a99aa43dfb0
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2017-07-01 16:51:33 +0200

    Populate fields if password field is present

--- content_scripts/main.js
+++ content_scripts/main.js
@@ -1,14 +1,25 @@
+const usernameFields = document.querySelectorAll("input[name*='username']");
+const passwordFields = document.querySelectorAll("input[type='password']");
 const url = window.top.location.host;
 
-request('passwords/search/access:' + encodeURIComponent(url))
-    .then(loadPasswordDetails)
-    .catch(handleError);
+if (passwordFields.length > 0) {
+    request('passwords/search/access:' + encodeURIComponent(url))
+        .then(loadPasswordDetails)
+        .catch(handleError);
+}
 
 function loadPasswordDetails(passwords) {
     passwords.forEach(password => {
         request('passwords/' + password.id).then(passwordDetail => {
-            console.log(passwordDetail)
+            populateFields(usernameFields, passwordDetail.username);
+            populateFields(passwordFields, passwordDetail.password)
         })
+    })
+}
+
+function populateFields(fields, value) {
+    fields.forEach(field => {
+        field.value = value;
     })
 }
 
