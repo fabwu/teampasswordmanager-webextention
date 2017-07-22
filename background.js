14aba51d9939c1d8208a6245899de19968d31aac
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2017-07-22 13:54:30 +0200

    Change icon when login successful

--- background.js
+++ background.js
@@ -6,15 +6,40 @@ function dispatch(message) {
             return search(message);
             break;
         case 'auth':
+            let result;
             if (message.args) {
-                return auth(message.args.username, message.args.password, message.args.url);
+                result = auth(message.args.username, message.args.password, message.args.url);
             } else {
-                return auth()
+                result = auth()
             }
+            return result
+                .then(turnIconActive)
+                .catch(turnIconInactive);
             break;
         default:
             throw new Error('Message type not found');
     }
+}
+
+function turnIconActive() {
+    browser.browserAction.setIcon({
+        path: {
+            16: 'icons/logo-16.png',
+            32: 'icons/logo-32.png',
+            48: 'icons/logo-48.png',
+        }
+    })
+}
+
+function turnIconInactive(e) {
+    browser.browserAction.setIcon({
+        path: {
+            16: 'icons/logo-16-grey.png',
+            32: 'icons/logo-32-grey.png',
+            48: 'icons/logo-48-grey.png',
+        }
+    });
+    throw e;
 }
 
 function search(message) {
