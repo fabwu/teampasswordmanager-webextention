8ca9f6d769f1ecc848957c1946617ad649ff25a9
Fabian Wüthrich <fabian.wuethrich@suxesiv.ch>
2017-12-04 21:33:06 +0100

    #5 Add icon to fields for opening popup

--- content_scripts/main.js
+++ content_scripts/main.js
@@ -1,4 +1,12 @@
 const passwordFields = document.querySelectorAll("input[type='password']");
+const usernameFields = [];
+
+passwordFields.forEach(passwordField => {
+    usernameFields.push(passwordField.form.querySelector("input[type='text']"));
+});
+
+passwordFields.forEach(addIconToField);
+usernameFields.forEach(addIconToField);
 
 if (passwordFields.length > 0) {
     browser.runtime
@@ -14,8 +22,18 @@ if (passwordFields.length > 0) {
 function fillInCredentials(auth) {
     passwordFields.forEach(passwordField => {
         passwordField.value = auth.password;
+    });
 
-        const usernameField = passwordField.form.querySelector("input[type='text']");
+    usernameFields.forEach(usernameField => {
         usernameField.value = auth.username;
     })
+}
+
+function addIconToField(field) {
+    const icon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4QcCFRkfBQCzagAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAABlUlEQVQ4y32TMUgcURCGv3m7osTYGEsLi4TgNgk2ImKVUgg5kxRezs4yHHYiaexsRSy9KgeXQr0UwTRpxBRiUgbOgBARQRAkRVKdt+9PsfG82711yjf/fDPzD8/oEePFnWcEwQowCYB0JLR2XJ37ktZaprhUL5uxDpbKSRJLjWphIxfwuLQ9FRB+xXCSdkW8kYiCspnNIbyPNX1cKxze1ISdAEewjOGQ9hrVwsuO1H60UP+E2awLbRko3NZ0bzQDIIs3s7v6ZHQx0920U2QMA5j3p2lA3HS/EpGGcwFtkAuVfYvVbpM2MXq92qeBJwuGVRK//ZrgsruTjWD2DsDLL7auzt+ffC43ExMHnhYNKreruBUjP5y5rfDBKEAlWcEzlBZJOlOr9Qg1HyJOe3CGMmdM2X7QqL06AYhK9QOwsV6yXICJ5+Pz25ELnZd4kae7a4L7Xte/g5ZiBfcGc/3IBYijnx/mL37UipcyHd49gUmZf2X0RW8+vv0P68+k5dQGSP674f5g1nENm8Ax0Xs4/jribwD/ACnZjCv36DmJAAAAAElFTkSuQmCC';
+    field.style.backgroundImage = "url('" + icon + "')";
+    field.style.backgroundRepeat = "no-repeat";
+    field.style.backgroundAttachment = "scroll";
+    field.style.backgroundSize = "16px 16px";
+    field.style.backgroundPosition = "calc(100% - 4px) 50%";
 }
